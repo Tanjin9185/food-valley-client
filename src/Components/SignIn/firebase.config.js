14e13e9e86fae047764b26c905c8e3ca1ada32bf
Tanjin Ahamed <tanjin.ahamed123@gmail.com>
2021-04-02 03:32:19 +0600

    client

---
+++ src/Components/SignIn/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBd83FMiOEInhpVXLDUVygploG8ZIhOIiE",
+  authDomain: "food-valley-2dcb2.firebaseapp.com",
+  projectId: "food-valley-2dcb2",
+  storageBucket: "food-valley-2dcb2.appspot.com",
+  messagingSenderId: "816308469016",
+  appId: "1:816308469016:web:d5aa1d19664770e313f26d"
+};
+  export default firebaseConfig;

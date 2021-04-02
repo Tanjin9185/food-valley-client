ff28f94d89d3aa576a381fb55b22c131831db00e
Tanjin Ahamed <tanjin.ahamed123@gmail.com>
2021-04-02 17:22:44 +0600

    almost done

--- src/Components/LoadProduct/LoadProduct.js
+++ src/Components/LoadProduct/LoadProduct.js
@@ -15,32 +15,9 @@ const LoadProduct = (props) => {
      history.push(url)
    }
     return (
-        // <>
-        //     <div className=" cards d-flex flex-wrap justify-content-between">
-        //     <div className="col-md-3 col-sm-12 ">
-        //         <Card className="singleCard" >
-        //             <Card.Img variant="top" className="img-fluid" src={imageURL} />
-        //             <Card.Body>
-        //                 <Card.Title > {name} </Card.Title>
-
-        //                 <Card.Text>
-        //                     <Row>
-        //                         <Col md={6}>
-        //                             Weight : {wight} <br />
-        //                             Price : ${price}
-        //                         </Col>
-        //                         <Col md={6} >
-        //                             {<Button className="btnStyle" variant="primary" onClick={()=>handleBuyNow(_id)}>Buy Now</Button>}
-        //                         </Col>
-        //                     </Row>
-        //                 </Card.Text>
-        //             </Card.Body>
-        //         </Card>
-        //     </div>
-        //     </div>
-        // </>
 
         <div className="container col-md-4">
+        <div className="">
         <Card className="custom shadow-sm p-2 m-4 bg-body rounded" style={{ width: '18rem' }}>
             <Card.Img variant="top" className="productImg" src={imageURL} />
             <Card.Body>
@@ -48,13 +25,14 @@ const LoadProduct = (props) => {
             </Card.Body>
             <div className="row">
                 <div className="col-md-6 ">
-                    <Card.Text><strong>Price: {price} Taka</strong> </Card.Text>
+                    <Card.Text><strong>Price: ${price}</strong> </Card.Text>
                 </div>
                 <div className="col-md-6 mb-4 App">
                     <Card.Link ><Button onClick={()=>handleBuyNow(_id)} >Buy Now</Button> </Card.Link>
                 </div>
             </div>
         </Card>
+        </div>
     </div>
 
     );
